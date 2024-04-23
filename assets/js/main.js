9ccce5115c3aa63a417c2ddc334bde3861bacd2f
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-04-23 22:05:00 +0600

    ui change navbar

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$("#textslider").owlCarousel({
+  loop: true,
+  autoplay: true,
+  nav: true,
+  dots: false,
+  navText: [
+    '<i class="fa-solid fa-angle-left"></i>',
+    '<i class="fa-solid fa-angle-right"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
