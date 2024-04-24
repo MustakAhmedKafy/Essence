a023da1edc94911e57c85c0f5d6c8852e44b0e54
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-04-24 23:06:17 +0600

    card done

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,27 @@ $("#textslider").owlCarousel({
   },
 });
 
+
+$("#brand-slider").owlCarousel({
+  loop: true,
+  autoplay: true,
+  nav: true,
+  dots: false,
+  navText: [
+    '<i class="fa-solid fa-angle-left"></i>',
+    '<i class="fa-solid fa-angle-right"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 3.5,
+    },
+    600: {
+      items: 2.5,
+    },
+    1000: {
+      items: 6.5,
+    },
+  },
+});
+
+
