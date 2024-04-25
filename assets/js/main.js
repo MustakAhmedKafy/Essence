b3bc0adaf841a0698bb580bf23bd9459f74eb8fa
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-04-25 23:34:37 +0600

    project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,6 @@ $("#textslider").owlCarousel({
   },
 });
 
-
 $("#brand-slider").owlCarousel({
   loop: true,
   autoplay: true,
@@ -43,4 +42,52 @@ $("#brand-slider").owlCarousel({
   },
 });
 
+$("#most-viewd-items").owlCarousel({
+  loop: true,
+  autoplay: true,
+  nav: true,
+  dots: false,
+  navText: [
+    '<i class="fa-solid fa-angle-left"></i>',
+    '<i class="fa-solid fa-angle-right"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 2,
+      margin: 10,
+    },
+    600: {
+      items: 4,
+      margin: 15,
+    },
+    1000: {
+      items: 6,
+      margin: 20,
+    },
+  },
+});
 
+$("#top-brands").owlCarousel({
+  loop: true,
+  autoplay: true,
+  nav: true,
+  dots: false,
+  navText: [
+    '<i class="fa-solid fa-angle-left"></i>',
+    '<i class="fa-solid fa-angle-right"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 3,
+      margin: 10,
+    },
+    600: {
+      items: 4,
+      margin: 15,
+    },
+    1000: {
+      items: 7,
+      margin: 20,
+    },
+  },
+});
